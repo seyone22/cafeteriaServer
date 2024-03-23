936c3e921ffbc86ae79356796446ac95e2c5b6c6
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-03-24 04:22:55 +0530

    made the api class csrf exempt

--- api_app/views.py
+++ api_app/views.py
@@ -1,6 +1,7 @@
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -8,10 +9,11 @@ from .serializers import ReviewSerializer
 
 
 # Create your views here.
-@method_decorator(csrf_exempt, name='post')
 class ReviewViews(APIView):
     permission_classes = (IsAuthenticated,)
 
+    @csrf_exempt
+    @api_view(['POST'])
     def post(self, request):
         serializer = ReviewSerializer(data=request.data)
         if serializer.is_valid():
