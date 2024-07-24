e68c8b7a4e86e16cbd54ef9dc1acf1f9a6ad3175
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-07-25 00:26:51 +0530

    Rollback changes to startup script

--- startup.sh
+++ startup.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-DJANGO_SUPERUSER_USERNAME=admin \
-DJANGO_SUPERUSER_PASSWORD=wkhWVx5k \
-DJANGO_SUPERUSER_EMAIL=admin@example.com \
-python manage.py createsuperuser --noinput && \
-python manage.py collectstatic --noinput && \
-gunicorn --workers 2 cafeteriaServer.wsgi
+python manage.py collectstatic && gunicorn --workers 2 cafeteriaServer.wsgi
