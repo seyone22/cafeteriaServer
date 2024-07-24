ff041281c0f3b07df37c116a4b6832e7b1cc91ac
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-07-25 00:20:27 +0530

    modified startup script to create a superuser.

--- startup.sh
+++ startup.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-python manage.py collectstatic && gunicorn --workers 2 cafeteriaServer.wsgi
+DJANGO_SUPERUSER_USERNAME=admin \
+DJANGO_SUPERUSER_PASSWORD=wkhWVx5k \
+DJANGO_SUPERUSER_EMAIL=admin@example.com \
+python manage.py createsuperuser --noinput && \
+python manage.py collectstatic --noinput && \
+gunicorn --workers 2 cafeteriaServer.wsgi
